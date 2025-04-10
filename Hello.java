9909c9a2032c76a79df3446060911a173dcecc9d
Purushotham <165474408+GADDAM-PURUSHOTHAM@users.noreply.github.com>
2025-04-10 05:42:45 +0000

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ class Hello{
 public static void main(String []args)
 {
 
-System.out.println("Hii How are You");
+System.out.println("Hii How are You");System.out.println("Hii How are You");
 System.out.println("HeLLO wHERE ARE yOU");
 
 }
